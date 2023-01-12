2d0980d34001e138dfbe922c4100172a47eb6ae9
shashank kumar singh <53405502+shashank12237@users.noreply.github.com>
2023-01-12 19:29:04 +0530

    first project

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Welcome To React</h2>
+     <p> <i> Now we r learning react...</i></p>
+    </div>
+  );
+}
+
+export default App;
