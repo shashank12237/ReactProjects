e3c600410b9649487668eaec5c50e17d230514cf
shashank kumar singh <53405502+shashank12237@users.noreply.github.com>
2023-01-13 00:56:13 +0530

    component helloworldcomponent

--- my-app/src/Components/HelloWorldComponent.js
+++ my-app/src/Components/HelloWorldComponent.js
@@ -1,7 +1,10 @@
-function HelloWorldComponent()
-{
-    return(
-    <h2>Hello world</h2>
-    );
+// function HelloWorldComponent()
+// {
+//     return(
+//     <h2>Hello world</h2>
+//     );
+// }
+const HelloWorldComponent = () =>{
+    return(<h2>Hello world we r our first component  HelloWorldComponent</h2>);
 }
 export default HelloWorldComponent;
